308c5ea76ec99428181610a15a8f500d546150f8
Jeonghyeon Park <dotsnangles@gmail.com>
2023-06-01 09:40:49 +0900

    FIX: more files included for bento service and docker run command fixed

--- containerize.sh
+++ containerize.sh
@@ -1,6 +1,12 @@
+### build dependencies for bentoml
 poetry install
 poetry shell
+dvc pull
 
+### save bento model, build bento, and containerize bento service
 python save_bento_model.py
 bentoml build
 bentoml containerize news-topic-keyphrase-generator-t5-base:latest
+
+### launch containerized bento service
+# docker run --gpus all -it --rm -p 3000:3000 news-topic-keyphrase-generator-t5-base:[TAG] serve
